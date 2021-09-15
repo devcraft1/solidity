a62d18b6203807640ce7bcc97dbdb5f1f68f29ab
devcraft1 <ukonudennis64@gmail.com>
2021-09-15 16:59:34 +0100

    contracts

---
+++ contracts/ApprovalConracts.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.18;
+
+
+contract ApprovalContract{
+    address public sender
+    address public receiver
+    address public constant approver = ;
+
+    function deposit(address _receiver) external payable{
+        require (msg.value >0)
+        sender = msg.sender;
+        receiver =_receiver
+    }
+
+    function viewApprover() external pure returns(address){
+        returns(approver)
+    }
+
+    function approve() external{
+        require(msg.sender == approver);
+        receiver.transfer(address(this).balance);
+    }
+}
