bf1700aee9e309b7b8a8ac73111542d7ab771881
devcraft1 <ukonudennis64@gmail.com>
2021-09-17 17:22:14 +0100

    inheritance

---
+++ inheritance/block.sol
@@ -0,0 +1,44 @@
+pragama solidity ^0.4.0
+
+contract inheritance{
+ address owner;
+ bool deceased;
+ uint money
+
+ constructor() public payable{
+     owner = msg.sender;
+     money = msg.value;
+     deceased = false;
+
+ }
+
+ modifier oneOwner{
+     require(msg.sender ==owner);
+     _;
+ }
+  modifier isDeceased{
+     require(msg.sender ==true);
+     _;
+ }
+
+ address[] wallets;
+
+ mapping(address =>uint)inheritance;
+
+ function setUp(address _wallet, uint _inheritance) public oneOwner{
+     wallets.push(_wallet);
+     inheritance(_wallet) - _inheritance;
+ }
+
+ function moneyPaid() private isDeceased{
+     for(uint i=0; i < wallets.length; i++ ){
+         wallets[i].transfer(inheritance[wallets[i]]);
+     }
+
+    function died() public oneOwner {
+        deceased =true
+        moneyPaid()
+    }
+ }
+
+}
